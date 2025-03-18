4d667ad267401fa8f32d476c1549cc34cc6c4fa5
Pranjal Mishra <pranjal.mishra0306@gmail.com>
2025-03-18 17:16:06 +0530

    initial structure

---
+++ models/llm.py
@@ -0,0 +1,27 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from google.cloud import aiplatform as vertexai
+from utils.config import REGIONS
+import random
+from google.generativeai import GenerativeModel
+load_dotenv()
+
+
+async def get_model(temperature=0.3, top_p=0.7, top_k=40, max_output_tokens=1024):
+    vertexai.init(project=os.getenv("PROJECT_ID"), location=REGIONS[random.randint(0, 25)])  # Initialize Vertex AI
+    
+    generation_config = {
+        "temperature": temperature,
+        "top_p": top_p,
+        "top_k": top_k,
+        "max_output_tokens": max_output_tokens,
+    }
+    
+    model = GenerativeModel(
+        "gemini-1.5-pro-001",
+        generation_config=generation_config
+    ) 
+    return model
+
+
---
+++ storage/db.py
@@ -0,0 +1,11 @@
+from google.cloud import firestore
+import os
+
+def get_async_database():
+    try:
+       db=firestore.Client.from_service_account_json(os.getenv("FIRESTORE_ACCOUNT_KEY_FILE"))
+       print("Connected to firestore database successfully")
+       return db
+    except Exception as e:
+        print("Error connecting to database")
+        raise e
